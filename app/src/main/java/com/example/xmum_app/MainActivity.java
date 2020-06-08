54c2cb64e50d263f65327bf94d24501511c3d08b
maplewong1998 <maplewong1998@gmail.com>
2020-06-08 20:40:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xmum_app/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.xmum_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 6000;
+
+    //Values
+    Animation topAnim, botAnim;
+    ImageView image;
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        botAnim = AnimationUtils.loadAnimation(this, R.anim.bot_anim);
+
+        //Hooks
+        image = findViewById(R.id.logo);
+        text = findViewById(R.id.slogan);
+
+        image.setAnimation(topAnim);
+        text.setAnimation(botAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginMain.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+    }
+}
