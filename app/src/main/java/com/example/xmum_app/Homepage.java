53df2ff8292cbc7a2cd44e17a9cb06878e0c72e7
maplewong1998 <maplewong1998@gmail.com>
2020-07-06 22:25:30 +0800

    New coursess entry function, tested and verified

--- app/src/main/java/com/example/xmum_app/Homepage.java
+++ app/src/main/java/com/example/xmum_app/Homepage.java
@@ -10,24 +10,41 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.material.navigation.NavigationView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class Homepage extends AppCompatActivity {
+
+public class Homepage extends AppCompatActivity implements StaffViewCourses.StaffViewCoursesListener{
+    private StaffViewCourses staffViewCourses;
     private SessionHandler session;
     private DrawerLayout drawerLayout;
+    private static final String KEY_STATUS = "status";
+    private static final String KEY_MESSAGE = "message";
+    private static final String KEY_COURSE_ID = "course_id";
+    private static final String KEY_COURSE_NAME = "course_name";
+    private static final String KEY_CREDIT = "credit";
+    private static final String KEY_LECTURER_ID = "lecturer_id";
+    private static final String KEY_STUDENT_NO = "student_no";
+    private String create_courses_url = "http://10.0.2.2:80/xmum_app_server/create_courses.php";
+    final LoadingDialog loadingDialog = new LoadingDialog(Homepage.this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home);
+
         session = new SessionHandler(getApplicationContext());
-        User user = session.getUserDetails();
-        final String privilege = user.getRole();
-        final String userID = user.getId();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("");
@@ -44,12 +61,9 @@ public class Homepage extends AppCompatActivity {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 int itemId = menuItem.getItemId();
-                if(itemId == R.id.nav_courses && privilege.equals("Staff")) {
-                    StaffViewCourses fragment = new StaffViewCourses();
-                    Bundle args = new Bundle();
-                    args.putString("UserID", userID);
-                    fragment.setArguments(args);
-                    replaceFragment(fragment);
+                if(itemId == R.id.nav_courses && session.getUserDetails().getRole().equals("Staff")) {
+                    staffViewCourses = new StaffViewCourses();
+                    replaceFragment(staffViewCourses);
                 }
                 else if(itemId == R.id.nav_logout) {
                     replaceFragment(new StudentViewCourses());
@@ -62,8 +76,8 @@ public class Homepage extends AppCompatActivity {
         TextView rnn_displayer = navigationView.getHeaderView(0).findViewById(R.id.role_and_name_displayer);
         TextView email_displayer = navigationView.getHeaderView(0).findViewById(R.id.email_displayer);
 
-        rnn_displayer.setText(user.getRole() + " " + user.getFullName());
-        email_displayer.setText(user.getEmail());
+        rnn_displayer.setText(session.getUserDetails().getRole() + " " + session.getUserDetails().getFullName());
+        email_displayer.setText(session.getUserDetails().getEmail());
     }
 
     @Override
@@ -79,5 +93,48 @@ public class Homepage extends AppCompatActivity {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.fragment_empty, fragment);
         transaction.commit();
+    }
+
+    @Override
+    public void onSVCInputSent(String CourseID, String CourseName, int Credit, int StudentNo) {
+        loadingDialog.startLoadingDialog();
+        JSONObject request = new JSONObject();
+        try {
+            //Populate the request parameters
+            request.put(KEY_COURSE_ID, CourseID);
+            request.put(KEY_COURSE_NAME, CourseName);
+            request.put(KEY_CREDIT, Credit);
+            request.put(KEY_LECTURER_ID, session.getUserDetails().getId());
+            request.put(KEY_STUDENT_NO, StudentNo);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest jsArrayRequest = new JsonObjectRequest
+                (Request.Method.POST, create_courses_url, request, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        loadingDialog.dismissLoadingDialog();
+                        try {
+                            Toast.makeText(getApplicationContext(), response.getString(KEY_MESSAGE), Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        loadingDialog.dismissLoadingDialog();
+
+                        //Display error message whenever an error occurs
+                        Toast.makeText(getApplicationContext(),
+                                error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsArrayRequest);
     }
 }
